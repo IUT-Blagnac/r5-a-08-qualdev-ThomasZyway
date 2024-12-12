faef423e3d081820850736cc8b5ff93cb8ea12d8
ThomasZyway <darkskirime@gmail.com>
2024-12-12 11:15:37 +0100

    udate tp2

--- cucumber_demo/src/test/java/dojo/RunCucumberTest.java
+++ cucumber_demo/src/test/java/dojo/RunCucumberTest.java
@@ -1,7 +1,8 @@
 package dojo;
 
-import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)  
 public class RunCucumberTest { }
